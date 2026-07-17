e941647e61a397f2e820ee1ed7dc2c0458805c9a
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2026-07-17 22:38:17 +0600

    Add AppConstants class for application-wide constants

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,13 @@
+class AppConstants {
+  AppConstants._();
+
+  static const String appName = 'EzyCourse';
+  static const String appversion = '1.0.0';
+
+  static const String apiBaseUrl = 'https://demo1.ezycourse.com/api/app';
+  // static const int tokenExpiryDays = 7;
+
+  static const String defaultLanguage = 'en';
+  static const String defaultDateFormat = 'dd/MM/yyyy';
+  static const String defaultTimeFormat = 'HH:mm';
+}
