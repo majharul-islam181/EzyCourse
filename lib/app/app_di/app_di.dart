9144f04abb98a322c0b913deb75b8560be47aeb0
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2026-07-18 13:01:52 +0600

    Add authentication dependency initialization in app_di

--- lib/app/app_di/app_di.dart
+++ lib/app/app_di/app_di.dart
@@ -1,5 +1,7 @@
 import '../../core/di/core_di.dart' as core_di;
+import '../../features/auth/di/auth_di.dart';
 
 Future<void> init() async {
   await core_di.initDependencies();
+  initAuthDependencies();
 }
