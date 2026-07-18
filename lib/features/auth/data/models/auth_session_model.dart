b424f5fd3b3cbb504c3cc1fecf2142457a2a065e
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2026-07-18 12:58:13 +0600

    Add AuthSessionModel to handle authentication session data

---
+++ lib/features/auth/data/models/auth_session_model.dart
@@ -0,0 +1,26 @@
+import '../../domain/entities/auth_session.dart';
+
+class AuthSessionModel extends AuthSession {
+  const AuthSessionModel({
+    required super.type, 
+    required super.token,
+    });
+
+  factory AuthSessionModel.fromJson(final Map<String, dynamic> json) {
+    return AuthSessionModel(
+      type: json['type'] as String,
+      token: json['token'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'type': type, 'token': token};
+  }
+
+  AuthSessionModel copyWith({final String? type, final String? token}) {
+    return AuthSessionModel(
+      type: type ?? this.type,
+      token: token ?? this.token,
+    );
+  }
+}
