7785a7e0b3bafd19c9756b1e75a49b7e180952ab
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2026-07-18 18:36:57 +0600

    Refactor dependency injection for coaching: add GetCoachingDetailsWithSessions use case and CoachingDetailsBloc

--- lib/features/coaching/di/coaching_di.dart
+++ lib/features/coaching/di/coaching_di.dart
@@ -3,7 +3,9 @@ import '../../../core/network/dio_client.dart';
 import '../data/datasources/coaching_remote_data_source.dart';
 import '../data/repositories/coaching_repository_impl.dart';
 import '../domain/repositories/coaching_repository.dart';
+import '../domain/usecases/get_coaching_details_with_sessions_usecase.dart';
 import '../domain/usecases/get_enrolled_coaching_programs_usecase.dart';
+import '../presentation/bloc/coaching_details_bloc.dart';
 import '../presentation/bloc/coaching_program_list_bloc.dart';
 
 void initCoachingDependencies() {
@@ -19,7 +21,15 @@ void initCoachingDependencies() {
     () => GetEnrolledCoachingProgramsUseCase(sl<CoachingRepository>()),
   );
 
+  sl.registerLazySingleton<GetCoachingDetailsWithSessionsUseCase>(
+    () => GetCoachingDetailsWithSessionsUseCase(sl<CoachingRepository>()),
+  );
+
   sl.registerFactory<CoachingProgramListBloc>(
     () => CoachingProgramListBloc(sl<GetEnrolledCoachingProgramsUseCase>()),
+  );
+
+  sl.registerFactory<CoachingDetailsBloc>(
+    () => CoachingDetailsBloc(sl<GetCoachingDetailsWithSessionsUseCase>()),
   );
 }
