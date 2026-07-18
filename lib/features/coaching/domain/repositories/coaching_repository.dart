ab07608617c78fbab08fed5e4107490340e5c8c8
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2026-07-18 14:56:06 +0600

    Add CoachingRepository interface to define method for fetching enrolled coaching programs

---
+++ lib/features/coaching/domain/repositories/coaching_repository.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failure.dart';
+import '../entities/coaching_program_page.dart';
+
+abstract class CoachingRepository {
+  Future<Either<Failure, CoachingProgramPage>> getEnrolledCoachingPrograms({
+    required int page,
+    required int perPage,
+  });
+}
