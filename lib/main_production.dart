78ad2e6cab706d68f08c763c28f34b8651cb36f3
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2026-07-18 13:48:03 +0600

    Fix import alias for app_di in main_production.dart

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'app/app.dart';
-import 'app/app_di/app_di.dart' as di;
+import 'app/app_di/app_di.dart' as app_di;
 import 'flavors/production.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   configureProduction();
-  await di.init();
+  await app_di.init();
   runApp(const MyApp());
 }
